fda6c7e19a72d788a5d25303a0be45e3500138b7
Luiz <luiz.nunes@amplifique.me>
2023-08-13 22:44:12 -0300

    windows support + icons added

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 document.onkeydown = keydown;
 
 function keydown(evt) {
-  if (evt.metaKey && evt.keyCode === 82) {
+  if ((evt.metaKey || evt.ctrlKey) && evt.keyCode === 82) {
     evt.preventDefault();
   }
 }
